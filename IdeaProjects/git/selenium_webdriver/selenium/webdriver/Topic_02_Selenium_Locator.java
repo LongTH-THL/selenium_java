9f791acec22c3aec225af85c259a2a892d0863fe
Trần Hải Long <tranhailong_t59@hus.edu.vn>
2021-08-30 00:03:15 +0700

    them TC

--- IdeaProjects/git/selenium_webdriver/selenium/webdriver/Topic_02_Selenium_Locator.java
+++ IdeaProjects/git/selenium_webdriver/selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -55,7 +55,49 @@ public class Topic_02_Selenium_Locator {
         Assert.assertEquals(TextPhone,"Vui lòng nhập số điện thoại.");
     }
 
+    @Test
+    public void TC_02_Register_With_Invalid_Email(){
+        // click to Name
+        WebElement Name = driver.findElement(By.xpath("//input[@name='txtFirstname']"));
+        Name.click();
+        Name.sendKeys("Tran Hai Long");
+        // click to Email
+        WebElement Email = driver.findElement(By.xpath("//input[@name='txtEmail']"));
+        Email.click();
+        Email.sendKeys("1234@1234.12@");
+        // click to CEmail
+        WebElement CEmail = driver.findElement(By.xpath("//input[@name='txtCEmail']"));
+        CEmail.click();
+        CEmail.sendKeys("1234@1234.12@");
+        // click to Pass
+        WebElement Pass = driver.findElement(By.xpath("//input[@name='txtPassword']"));
+        Pass.click();
+        Pass.sendKeys("123456789");
+        // clickCPass
+        WebElement CPass = driver.findElement(By.xpath("//input[@name='txtCPassword']"));
+        CPass.click();
+        CPass.sendKeys("123456789");
+        // click Phone
+        WebElement Phone = driver.findElement(By.xpath("//input[@name='txtPhone']"));
+        Phone.click();
+        Phone.sendKeys("0384453261");
+        // click Button dang ky
+        WebElement buttonRegister = driver.findElement(By.xpath("//button[@class='btn_pink_sm fs16']"));
+        buttonRegister.click();
+        // kiem tra noti thong bao
+        // Mail
+        WebElement notiEmail = driver.findElement(By.xpath("//label[@id='txtEmail-error']"));
+        String TextMail =notiEmail.getText();
+        Assert.assertEquals(TextMail,"Vui lòng nhập email hợp lệ");
+        // Confirm Mail
+        WebElement notiConfirmMail = driver.findElement(By.xpath("//label[@id='txtCEmail-error']"));
+        String TextCMail = notiConfirmMail.getText();
+        Assert.assertEquals(TextCMail,"Email nhập lại không đúng");
+    }
+    @Test
+    public void TC_03_(){
 
+    }
 
 
 
