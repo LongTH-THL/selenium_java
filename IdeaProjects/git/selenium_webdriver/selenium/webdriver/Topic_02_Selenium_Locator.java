174d37fb8f45ff83a1617848c160144829caab05
Trần Hải Long <tranhailong_t59@hus.edu.vn>
2021-08-30 19:18:10 +0700

    them TC ngay 30/08

--- IdeaProjects/git/selenium_webdriver/selenium/webdriver/Topic_02_Selenium_Locator.java
+++ IdeaProjects/git/selenium_webdriver/selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -95,11 +95,105 @@ public class Topic_02_Selenium_Locator {
         Assert.assertEquals(TextCMail,"Email nhập lại không đúng");
     }
     @Test
-    public void TC_03_(){
+    public void TC_03_Register_With_Incorect_Confirm_Email(){
 
+        // click to Email
+        WebElement Email = driver.findElement(By.xpath("//input[@name='txtEmail']"));
+        Email.clear();
+        Email.click();
+        Email.sendKeys("tranhailong_t59@hus.edu.vn");
+        // click to CEmail
+        WebElement CEmail = driver.findElement(By.xpath("//input[@name='txtCEmail']"));
+        CEmail.clear();
+        CEmail.click();
+        CEmail.sendKeys("1234@gmail.com");
+
+        // click Button dang ky
+        WebElement buttonRegister = driver.findElement(By.xpath("//button[@class='btn_pink_sm fs16']"));
+        buttonRegister.click();
+        // Kiem tra noti thong bao
+        // Confirm Mail
+        WebElement notiConfirmMail = driver.findElement(By.xpath("//label[@id='txtCEmail-error']"));
+        String TextCMail = notiConfirmMail.getText();
+        Assert.assertEquals(TextCMail,"Email nhập lại không đúng");
     }
 
+    @Test
+    public void TC_04_TC_03_Register_With_Password(){
 
+        // click to CEmail
+        WebElement CEmail = driver.findElement(By.xpath("//input[@name='txtCEmail']"));
+        CEmail.clear();
+        CEmail.click();
+        CEmail.sendKeys("tranhailong_t59@hus.edu.vn");
+        // click to Pass
+        WebElement Pass = driver.findElement(By.xpath("//input[@name='txtPassword']"));
+        Pass.clear();
+        Pass.click();
+        Pass.sendKeys("12345");
+        // clickCPass
+        WebElement CPass = driver.findElement(By.xpath("//input[@name='txtCPassword']"));
+        CPass.clear();
+        CPass.click();
+        CPass.sendKeys("12345");
+
+        // click Button dang ky
+        WebElement buttonRegister = driver.findElement(By.xpath("//button[@class='btn_pink_sm fs16']"));
+        buttonRegister.click();
+        // Kiem tra noti thong bao
+        // Pass < 6
+        WebElement notiPass = driver.findElement(By.xpath("//label[@id='txtPassword-error']"));
+        String TextPass = notiPass.getText();
+        Assert.assertEquals(TextPass,"Mật khẩu phải có ít nhất 6 ký tự");
+        // Confirm Pass < 6
+        WebElement notiConfirmPass = driver.findElement(By.xpath("//label[@id='txtCPassword-error']"));
+        String TextCPass = notiConfirmPass.getText();
+        Assert.assertEquals(TextCPass,"Mật khẩu phải có ít nhất 6 ký tự");
+    }
+
+    @Test
+    public void TC_05_Register_With_Incorect_Confirm_Password(){
+        // click to Pass
+        WebElement Pass = driver.findElement(By.xpath("//input[@name='txtPassword']"));
+        Pass.clear();
+        Pass.click();
+        Pass.sendKeys("123456789");
+        // clickCPass
+        WebElement CPass = driver.findElement(By.xpath("//input[@name='txtCPassword']"));
+        CPass.clear();
+        CPass.click();
+        CPass.sendKeys("1234567");
+
+        // click Button dang ky
+        WebElement buttonRegister = driver.findElement(By.xpath("//button[@class='btn_pink_sm fs16']"));
+        buttonRegister.click();
+        // Kiem tra noti thong bao
+        // Confirm Pass
+        WebElement notiConfirmPass = driver.findElement(By.xpath("//label[@id='txtCPassword-error']"));
+        String TextCPass = notiConfirmPass.getText();
+        Assert.assertEquals(TextCPass,"Mật khẩu bạn nhập không khớp");
+    }
+
+    @Test
+    public void TC_06_Register_With_Invalid_Phone_Number(){
+        // clickCPass
+        WebElement CPass = driver.findElement(By.xpath("//input[@name='txtCPassword']"));
+        CPass.clear();
+        CPass.click();
+        CPass.sendKeys("123456789");
+        // click Phone
+        WebElement Phone = driver.findElement(By.xpath("//input[@name='txtPhone']"));
+        Phone.click();
+        Phone.sendKeys("0384abcd");
+        // click Button dang ky
+        WebElement buttonRegister = driver.findElement(By.xpath("//button[@class='btn_pink_sm fs16']"));
+        buttonRegister.click();
+        // Kiem tra noti thong bao
+        // Noti phone
+        WebElement notiPhone = driver.findElement(By.xpath("//label[@id='txtPhone-error']"));
+        String TextCPass = notiPhone.getText();
+        Assert.assertEquals(TextCPass,"Vui lòng nhập con số");
+    }
 
 
 
@@ -107,7 +201,6 @@ public class Topic_02_Selenium_Locator {
     @AfterClass
     public void afterClass()
     {
-
         driver.quit();
     }
 }
